182616a3ad6f990250d584675dfeb900e74a2b9d
김민지 <133857306+minzlzl@users.noreply.github.com>
2024-05-16 12:17:28 +0900

    Delete script directory

--- script/swiper.js
+++
@@ -1,9 +0,0 @@
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 1,
-//     //spaceBetween: 10,
-//     loop: true,
-//     autoplay: {
-//         delay: 5000,
-//         disableOnInteraction: false,
-//     }
-// });
